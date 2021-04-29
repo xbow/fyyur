62691c4ab3c00838be92040d197fc61c91f9c303
Sebastian Armbrust <s.armbrust@mmkh.de>
2021-04-29 08:45:35 +0200

    Setup local db

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
+# Disable track modifications
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 # Connect to the database
-
-
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5423/fyyur2'
