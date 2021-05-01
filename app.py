962adde11c8f135dcb504c4a3d833dfb93032120
Sebastian Armbrust <s.armbrust@mmkh.de>
2021-05-01 12:18:49 +0200

    Implement /venues controller

--- app.py
+++ app.py
@@ -59,46 +59,33 @@ def index():
 
 @app.route('/venues')
 def venues():
-  # TODO: replace with real venues data.
-  #       num_shows should be aggregated based on number of upcoming shows per venue.
-
   # sort venues by state, then city, so rows with identical city+state will be adjacent
-  venues = Venue.query.order_by(Venue.state.asc()).order_by(Venue.city.asc()).all()
-  
-  data = []
-  for venue in venues:
-    data.append({
-      'city': venue.city,
-      'state': venue.state,
-      'venues': [{
-        'id': venue.id,
-        'name': venue.name,
-        'num_upcoming_shows': Show.query.filter_by(venue_id=venue.id).filter(Show.start_time>datetime.now()).count()
-      }]
-    })
+  sorted_venues = Venue.query.order_by(Venue.state.asc()).order_by(Venue.city.asc()).all()
 
-  # data=[{
-  #   "city": "San Francisco",
-  #   "state": "CA",
-  #   "venues": [{
-  #     "id": 1,
-  #     "name": "The Musical Hop",
-  #     "num_upcoming_shows": 0,
-  #   }, {
-  #     "id": 3,
-  #     "name": "Park Square Live Music & Coffee",
-  #     "num_upcoming_shows": 1,
-  #   }]
-  # }, {
-  #   "city": "New York",
-  #   "state": "NY",
-  #   "venues": [{
-  #     "id": 2,
-  #     "name": "The Dueling Pianos Bar",
-  #     "num_upcoming_shows": 0,
-  #   }]
-  # }]
-  return render_template('pages/venues.html', areas=data);
+  data = []
+
+  previous_city_state = {}
+
+  for venue in sorted_venues:
+    city_state = { 'city': venue.city, 'state': venue.state }
+
+    # create a new section only if the city_state dict has changed
+    if city_state != previous_city_state:
+      data.append({
+        'city': city_state['city'],
+        'state': city_state['state'],
+        'venues': []
+      })
+      previous_city_state = city_state
+
+    # in any case, append a venue to the last section
+    data[-1]['venues'].append({
+      'id': venue.id,
+      'name': venue.name,
+      'num_upcoming_shows': Show.query.filter_by(venue_id=venue.id).filter(Show.start_time>datetime.now()).count()
+    })      
+
+  return render_template('pages/venues.html', areas=data)
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
